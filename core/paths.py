1c7922dd7275ad22c59f7428c508d747109694e0
gusmmm <jgustavomartins@gmail.com>
2025-03-13 10:00:59 +0000

    Update .gitignore to include credentials and common ignores; add credentials directory to ProjectPaths

--- core/paths.py
+++ core/paths.py
@@ -22,6 +22,9 @@ class ProjectPaths:
         self.FRONTEND = self.ROOT / "frontend"
         self.BACKEND = self.ROOT / "backend"
         
+        # Add credentials directory
+        self.CREDENTIALS = self.ROOT / "credentials"
+        
         # Utils subdirectories based on project requirements
         self.DATA_TOOLS = self.UTILS / "data_tools"
         
@@ -44,7 +47,8 @@ class ProjectPaths:
             self.UTILS, self.DATA_TOOLS, 
             self.DATA, self.FRONTEND, self.BACKEND,
             self.DATA_SOURCE, self.PDF_SOURCE, self.SPREADSHEET_SOURCE,
-            self.DATA_PROCESSED, self.MARKDOWN, self.JSON
+            self.DATA_PROCESSED, self.MARKDOWN, self.JSON,
+            self.CREDENTIALS,
         ]
         
         for directory in directories:
